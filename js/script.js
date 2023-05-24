f0eaa46a27365fc30b24f31d34e93b1a033f7c1c
Unblockergod <101749215+Unblockergod@users.noreply.github.com>
2023-05-24 09:58:07 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@
 var itemCounter = 0;
 var fps = 15;
 
-var money = 7.50;
+var money = 7,000;
 var username = localStorage.getItem('username');
 if (localStorage.getItem("username") === null) {
 	localStorage.setItem('username' , "Player 1 (You)")
@@ -15,7 +15,7 @@ if (localStorage.getItem("imgData") === null) {
 }
 
 var currentCase = "case1";
-var acceptMoneyPerClick = 0.1;
+var acceptMoneyPerClick = 250,000;
 
 /*=========================Inventory============================*/
 //In inventory: weap skins
